b9a332e2595d6df40c7701ea8f4adb0cc9aa51e2
sledch8 <sledch8@furman.edu>
2024-09-24 19:58:35 -0400

    Completed Lab 3

--- app/src/main/java/pa3/Queue.java
+++ app/src/main/java/pa3/Queue.java
@@ -5,11 +5,20 @@ package pa3;
  * A queue of nodes.
  */
 public class Queue {
+    Node value;
+    Node[] queue;
+    int tracker;
+    int capacity;
 
     /** 
      * Constructs an empty queue.
      */
     public Queue() {
+        this.tracker = 0;
+        this.capacity = 10;
+        this.queue = new Node[this.capacity];
+
+        
 
         
     }
@@ -19,8 +28,26 @@ public class Queue {
      * @param node
      */
     public void enqueue(Node node) {
-
+        if (this.tracker == 0){
+            this.queue[0] = node;
+            this.tracker = this.tracker + 1;
+        }
         
+        else if (this.tracker == this.capacity){
+            //int total = this.capacity;
+            Node[] copylist = new Node[this.capacity * 2];
+            for (int i = 0; i < this.capacity; i++){
+                copylist[i] = this.queue[i];
+            }
+            this.queue = copylist;
+            this.capacity = this.capacity * 2;
+        }
+
+        else{
+            this.queue[this.tracker] = node;
+            this.tracker = this.tracker + 1;
+        }
+
     }
 
     /**
@@ -28,6 +55,13 @@ public class Queue {
      * @return the node at the front of the queue.
      */
     public Node dequeue() {
+        Node dequeued = this.queue[0];
+        for (int i = 0; i < this.tracker-1; i++){
+            this.queue[i] = this.queue[i+1];
+        }
+        this.tracker = this.tracker - 1;
+        return dequeued;
+
 
         
     }
@@ -37,15 +71,25 @@ public class Queue {
      * @return true if the queue is empty.
      */
     public boolean isEmpty() {
+        if (this.tracker == 0){
+            return true;
+        }
+
+        else{
+            return false;
+        }
         
     }
 
     public static void main(String[] args) {
         Queue queue = new Queue();
         Node node1 = new Node(4);
-        Node node2 = new Node(4);
+        Node node2 = new Node(3);
+        Node node3 = new Node(2);
         queue.enqueue(node1);
         queue.enqueue(node2);
+        queue.enqueue(node3);
+        queue.dequeue();
         System.out.println(queue.dequeue().value); // Should print 4
     }
     
--- app/src/test/java/pa3/BinaryTreeTest.java
+++ app/src/test/java/pa3/BinaryTreeTest.java
@@ -18,4 +18,145 @@ public class BinaryTreeTest {
         tree.add(7);
         assertEquals("1 2 3 4 5 6 7 ", tree.levelOrderTraversal());
     }
+
+    @Test
+    public void testLevelOrderTraversal2() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        tree.add(8);
+        tree.add(9);
+        tree.add(10);
+        tree.add(11);
+        tree.add(12);
+        tree.add(13);
+        tree.add(14);
+        assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 ", tree.levelOrderTraversal());
+    }
+
+    @Test
+    public void testLevelOrderTraversal3() {
+        BinaryTree tree = new BinaryTree();
+        assertEquals("", tree.levelOrderTraversal());
+    }
+
+    @Test
+    public void testinvert() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        tree.invert();
+        assertEquals("1 3 2 7 6 5 4 ", tree.levelOrderTraversal());
+    }
+
+    @Test
+    public void testinvert2() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.invert();
+        assertEquals("1 3 2 4 ", tree.levelOrderTraversal());
+    }
+
+    @Test
+    public void testinvert3() {
+        BinaryTree tree = new BinaryTree();
+        tree.invert();
+        assertEquals("", tree.levelOrderTraversal());
+    }
+
+    @Test
+    public void testenqueuelength() {
+        //Tests to see if the queue grows with the size of the list.
+        Queue queue = new Queue();
+        Node node1 = new Node(4);
+        Node node2 = new Node(3);
+        Node node3 = new Node(2);
+        Node node4 = new Node(4);
+        Node node5 = new Node(3);
+        Node node6 = new Node(2);
+        Node node7 = new Node(4);
+        Node node8 = new Node(3);
+        Node node9 = new Node(2);
+        Node node10 = new Node(4);
+        Node node11 = new Node(3);
+        Node node12 = new Node(2);
+
+        queue.enqueue(node1);
+        queue.enqueue(node2);
+        queue.enqueue(node3);
+        queue.enqueue(node4);
+        queue.enqueue(node5);
+        queue.enqueue(node6);
+        queue.enqueue(node7);
+        queue.enqueue(node8);
+        queue.enqueue(node9);
+        queue.enqueue(node10);
+        queue.enqueue(node11);
+        queue.enqueue(node12);
+        assertEquals(20, queue.capacity);
+    }
+
+    @Test
+    public void testgetHeight() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        assertEquals(2, tree.getHeight());
+    }
+
+    @Test
+    public void testgetHeight2() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        tree.add(8);
+        tree.add(9);
+        tree.add(10);
+        tree.add(11);
+        tree.add(12);
+        tree.add(13);
+        tree.add(14);
+        assertEquals(3, tree.getHeight());
+    }
+
+    @Test
+    public void testgetHeight3() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        assertEquals(0, tree.getHeight());
+    }
+
+    @Test
+    public void testgetHeight4() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        assertEquals(1, tree.getHeight());
+    }
+
+
 }
